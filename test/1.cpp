c083938d85cc5bf0ff6543e9f00abdd712436272
YaeSaraki <ikaraseay@iCloud.com>
2023-03-03 17:03:06 +0800

    调整结构

--- test/1.cpp
+++ test/1.cpp
@@ -1,51 +1,27 @@
-// 4 + 3
-// 7 5 3 3
-//  + * +
 
-#include <algorithm>
-#include <fstream>
 #include <iostream>
-#include <stack>
-#include <map>
-
+#include <algorithm>
 using namespace std;
-
-map<char, int> priority;
-bool isnum(char ch);
-bool isoperator(char ch);
-int compare(char a, char b);
-double cal(double a, double b, char op);
-
-
+int ToS(int h, int m, int s, int t) {
+  int time = h * 3600 + m * 60 + s + t * (3600 * 24);
+  return time;
+}
 int main() {
-	
+  int T; cin >> T; while(T--) {
+    int h_1, m_1, s_1, h_2, m_2, s_2, t1, t2, t3, t4, t = 0;
+    scanf("%d:%d:%d %d:%d:%d (+%d)", &h_1, &m_1, &s_1, &h_2, &m_2, &s_2, &t);
+    t1 = ToS(h_1, m_1, s_1, 0);
+    t2 = ToS(h_2, m_2, s_2, t); 
+    t3 = t2 - t1;
+    t = 0;
+    scanf("%d:%d:%d %d:%d:%d (+%d)", &h_1, &m_1, &s_1, &h_2, &m_2, &s_2, &t);
+    t1 = ToS(h_1, m_1, s_1, 0);
+    t2 = ToS(h_2, m_2, s_2, t);
+    t = 0;
+    t4 = t2 - t1;
+    t2 = (t3 + t4) / 2;
+    h_1 = t2 / 3600, m_1 = t2 % 3600 / 60, s_1 = t2 % 60;
+    printf("%02d:%02d:%02d\n", h_1, m_1, s_1);
+  }
+  return 0; 
 }
-
-bool isnum(char ch) {
-  if ('0' <= ch || ch <= '9') return true;
-  return false;
-}
-
-bool isoperator(char ch) {
-  if (ch == '+' || ch == '-' || ch == '*' || ch =='/') return true;
-  return false;
-}
-
-int compare(char a, char b) {
-	int cmp = 0;
-	if (priority[a] <= priority[b]) cmp = -1;
-	if (priority[a] > priority[b]) cmp = 1;
-	return cmp;  
-}
-
-double cal(double a, double b, char op) {
-	double ans;
-	switch (op) {
-		case '+' : ans = a + b; break;
-		case '-' : ans = a - b; break;
-		case '*' : ans = a * b; break;
-		case '/' : ans = a / b; break;
-	}
-	return ans;
-}
-
